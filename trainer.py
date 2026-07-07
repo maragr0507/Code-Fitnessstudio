2b41d2b7540dcc1ac4edc931a98b9d31adacf11b
maragr0507 <mara.griebe@gmx.de>
2026-07-07 15:28:01 +0200

    Änderung eines Rechtschreibfehlers

--- trainer.py
+++ trainer.py
@@ -1,7 +1,7 @@
 from person import Person
 class trainer(Person):
-    def __init__( self,name, alter,geburtdatum,telefon,email,personalnummer, fachgebiet, kurse):
-       super().__init__(name,alter,geburtdatum,telefon,email )
+    def __init__( self,name, alter,geburtsdatum,telefon,email,personalnummer, fachgebiet, kurse):
+       super().__init__(name,alter,geburtsdatum,telefon,email )
 
        self.personalnummer=personalnummer
        self.fachgebiet=fachgebiet
@@ -11,4 +11,4 @@ class trainer(Person):
         self.kurs.append(kurs)
     def kurs_entfernen(self,kurs):
         self.kurs.remove(kurs)
-        
+
