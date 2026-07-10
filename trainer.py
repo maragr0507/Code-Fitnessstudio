d03ae9909af1bded25c4ebe4dd3b8cbedf43fc6d
maragr0507 <mara.griebe@gmx.de>
2026-07-10 17:02:25 +0200

    Einfügen der Kommentare und Fehleranpassung

--- trainer.py
+++ trainer.py
@@ -1,14 +1,35 @@
+"""
+trainer.py
+
+Ein Trainer hat:
+- alle Eigenschaften einer Person
+- eine Personalnummer
+- ein Fachgebiet
+- eine Liste der betreuten Kurse
+"""
+
 from person import Person
-class trainer(Person):
-    def __init__( self,name, alter,geburtsdatum,telefon,email,personalnummer, fachgebiet, kurse):
+
+class Trainer(Person):
+    """Die Klasse Trainer beschreibt einen Trainer im Fitnessstudio."""
+    def __init__( self,name, alter,geburtsdatum,telefon,email,personalnummer, fachgebiet):
+       
+       # Eigenschaften von der Klasse Person werden uebernommen
        super().__init__(name,alter,geburtsdatum,telefon,email )
 
+       # Eindeutige Personalnummer
        self.personalnummer=personalnummer
+       
+       # Fachgebiet des Trainers
        self.fachgebiet=fachgebiet
+
+       # Liste aller Kurse, die der Trainer betreut
        self.kurse=[]
 
     def kurs_uebernehmen(self,kurs):
-        self.kurs.append(kurs)
+        """Fuegt einen Kurs zur Liste hinzu."""
+        self.kurse.append(kurs)
     def kurs_entfernen(self,kurs):
-        self.kurs.remove(kurs)
+        """Entfernt einen Kurs aus der Liste."""
+        self.kurse.remove(kurs)
 
