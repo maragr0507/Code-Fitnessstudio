bc6198c3c4e3cd071e093e2f8b496fb3425e054b
maragr0507 <mara.griebe@gmx.de>
2026-07-10 16:54:04 +0200

    Einfügen von Kommentaren

--- fitnessstudioexception.py
+++ fitnessstudioexception.py
@@ -1,6 +1,15 @@
+"""
+fitnessstudioexception.py
+
+Eigene Exception-Klasse fuer Fehler im Fitnessstudio-System.
+"""
 class FitnessstudioException(Exception): 
+    """Diese Klasse beschreibt benutzerdefinierte Fehler im Fintnessstudio."""
     def __init__( self,message ):
+        # Fehlermeldung wird gespeichert. 
         self.message= message
+
     def __str__(self):
+        """Gibt die Fehlermeldung als Text zurueck."""
         return self.message 
     
