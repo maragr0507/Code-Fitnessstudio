7f32dfa953bb69d7c0f0addfa53916998d6a8667
maragr0507 <mara.griebe@gmx.de>
2026-07-10 14:50:49 +0200

    Füllen der Klasse mitgliedschaft.py mit Anpassung der Kommentare

---
+++ mitgliedschaft.py
@@ -0,0 +1,24 @@
+"""
+mitgliedschaft.py
+
+Die Klasse Mitgliedschaft beschreibt eine Mitgliedschaft im Fitnessstudio.
+
+"""
+class Mitgliedsschaft: 
+    def __init__(self,typ,preis):
+        # Typ der Mitgliedschaft
+        self.typ=typ
+
+        #Monatlicher Preis der Mitgliedschaft
+        self.preis=preis
+
+        # Liste mit allen erlaubten leistungen 
+        self.erlaubte_leistungen=[]
+
+        # Prüft, ob eine bestimmte Leistung erlaubt ist
+    def hat_leistung(self,leistung):
+        """ 
+        Rueckgabe:
+        True  -> Die Leistung ist enthalten.
+        False -> Die Leistung ist nicht enthalten."""
+        return leistung in self.erlaubte_leistungen 
