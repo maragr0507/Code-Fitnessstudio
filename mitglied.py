9c5588bc0accafd2fad08ff9435736fc364a6878
maragr0507 <mara.griebe@gmx.de>
2026-07-10 16:48:36 +0200

    Fehlerkorrektur und einfügen von Kommentaren

--- mitglied.py
+++ mitglied.py
@@ -1,20 +1,41 @@
 from person import Person
-class Mitglied (Person):
-    def __init__(self,name, alter,geburtdatum,telefon,email, mitgliedsnummer, mitgliedschaft, gebuchte_kurse,reservierte_geraete):
-        super().__init__(name,alter,geburtdatum,telefon,email )
+"""
+mitglied.py 
 
-        self.mitliedsnummer=mitgliedsnummer
+Ein Mitglied hat:
+- alle Eigenschaften einer Person
+- eine Mitgliedsnummer
+- eine Mitgliedschaft
+- eine Liste gebuchter Kurse
+- eine Liste reservierter Geraete
+
+"""
+class Mitglied(Person):
+    """Die Klasse Mitglied beschreibt ein Mitglied im Fitnessstudio"""
+    def __init__(self,name, alter,geburtsdatum,telefon,email, mitgliedsnummer, mitgliedschaft):
+        # Eigenschaften der Klasse Person 
+        super().__init__(name,alter,geburtsdatum,telefon,email )
+
+        # Eindeutige Mitgliedsnummer 
+        self.mitgliedsnummer=mitgliedsnummer
+
+        # Mitgliedschaft des Mitglieds 
         self.mitgliedschaft=mitgliedschaft
-        #"""Ein Mitglied kann mehrere Kurse buchen und mehrere Geräte reservieren. Dafür eignet sich eine Liste."""
+
+        # Ein Mitglied kann mehrere Kurse buchen und mehrere Geräte reservieren. Dafür eignet sich eine Liste.
         self.gebuchte_kurse = []
         self.reservierte_geraete = []
 
     def kurs_buchen(self,kurs):
+            """Fuegt einen Kurs zu den gebuchten Kursen hinzu. """
             self.gebuchte_kurse.append(kurs)
     def kurs_stornieren(self, kurs):
+            """Entfernt einen Kurs aus den gebuchten Kursen."""
             self.gebuchte_kurse.remove(kurs)
     def geraet_reservieren(self, geraet):
+            """Reserviert ein gerät für das Mitglied"""
             self.reservierte_geraete.append(geraet)
     def geraet_freigeben(self, geraet):
-            self.reservierte_geraet.remove(geraet)
+            """Gibt ein reserviertes Gerät wieder frei"""
+            self.reservierte_geraete.remove(geraet)
 
