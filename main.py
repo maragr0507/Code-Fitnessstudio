4122aca3b1fe23bbeccf1abce1a840dedb098428
maragr0507 <mara.griebe@gmx.de>
2026-07-11 12:41:39 +0200

    Importieren der verschiedenen Klassen

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+main.py 
+
+Startdatei des Fitnesstudio-Programms.
+
+Hier werden Beispielobjekte erstellt und 
+"""
+
+from mitgliedschaft import Mitgliedsschaft
+from mitglied import Mitglied
+from trainer import Trainer 
+from kurs import Kurs 
+from fitnessstudioexception import FitnessstudioException
+
+
